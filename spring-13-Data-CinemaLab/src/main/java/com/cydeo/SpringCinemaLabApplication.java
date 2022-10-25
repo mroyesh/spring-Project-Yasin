32db4b6671ef03c906a1ea0397bc65e867011b2d
yasinTawasulli <royesh14@gmail.com>
2022-10-25 12:23:17 -0700

    CinemaLab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/SpringCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/SpringCinemaLabApplication.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,9 +19,9 @@ public class SpringCinemaLabApplication {
 
     }
 
-//    @Bean
-//    public MigrateResult migrateResult(DataSource dataSource){
-//        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-//    }
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
 
 }
